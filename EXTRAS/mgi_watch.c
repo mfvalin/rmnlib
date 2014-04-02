e0a300a76a06be6f8f6d154b1cbf4c8b59cbf1d9
Michel <armnmfv@nemesis.(none)>
2014-04-02 10:39:09 -0400

    monitoring program for  shared memory channel

--- EXTRAS/mgi_watch.c
+++ EXTRAS/mgi_watch.c
@@ -15,20 +15,42 @@ main(int argc, char **argv)
   int nrep = 100;
   int i;
   int delay=2;
+  int attach,in,out,read_status,write_status;
 
   if(argc<2) exit(1);
   shmid = atoi(argv[1]);
   if(argc>2) delay=atoi(argv[2]);
+
   shm = shmat(shmid, NULL, 0);
   if(shm == (void *) -1) exit (1);
-  while(nrep-- > 0) {
-    if(-1 == shmctl(shmid,IPC_STAT,&shm_stat)) exit (0);
+
+  if(-1 == shmctl(shmid,IPC_STAT,&shm_stat)) exit (0);
+  fprintf(stderr,"size = %d, attach count = %d, creator=%d, last attach=%d ",
+          (int)shm_stat.shm_segsz,(int)shm_stat.shm_nattch,shm_stat.shm_cpid,shm_stat.shm_lpid);
+  fprintf(stderr,"first=%d, in=%d, out=%d, limit=%d, rs=%d, ws=%d\n",
+          shm->first,shm->in,shm->out,shm->limit,shm->read_status,shm->write_status);
+  in = shm->in;
+  out = shm->out;
+  read_status = shm->read_status;
+  write_status = shm->write_status;
+  attach = shm_stat.shm_nattch;
+
+  while(1) {   /* loop until shared memory area disappears */
+    usleep(1000);   /* 1 millisec sleep */
+    if(-1 == shmctl(shmid,IPC_STAT,&shm_stat)) exit (0);  /* shared memory area has disappeared */
     /* TODO: add a time tag to get a good idea of traffic timing */
-    /* TODO: use a dynamic watcher that talks only when there are changes */
-    fprintf(stderr,"size = %d, attach count = %d, creator=%d, last attach=%d ",
-            (int)shm_stat.shm_segsz,(int)shm_stat.shm_nattch,shm_stat.shm_cpid,shm_stat.shm_lpid);
-    fprintf(stderr,"first=%d, in=%d, out=%d, limit=%d, rs=%d, ws=%d\n",
-            shm->first,shm->in,shm->out,shm->limit,shm->read_status,shm->write_status);
-    sleep(delay);
+    if(in != shm->in || out != shm->out || 
+       read_status != shm->read_status || write_status != shm->write_status || 
+       attach != shm_stat.shm_nattch){
+          fprintf(stderr,"size = %d, attach count = %d, creator=%d, last attach=%d ",
+                  (int)shm_stat.shm_segsz,(int)shm_stat.shm_nattch,shm_stat.shm_cpid,shm_stat.shm_lpid);
+          fprintf(stderr,"first=%d, in=%d, out=%d, limit=%d, rs=%d, ws=%d\n",
+                  shm->first,shm->in,shm->out,shm->limit,shm->read_status,shm->write_status);
+    }
+    in = shm->in;
+    out = shm->out;
+    read_status = shm->read_status;
+    write_status = shm->write_status;
+    attach = shm_stat.shm_nattch;
   }
 }
