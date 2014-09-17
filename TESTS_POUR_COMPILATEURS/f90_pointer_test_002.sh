15ed9f4956db922bf7cfabf327a53550bfaff3a5
Michel Valin <valin.michel@sca.uqam.ca>
2014-09-17 17:01:10 -0400

    another pointer test

---
+++ TESTS_POUR_COMPILATEURS/f90_pointer_test_002.sh
@@ -0,0 +1,56 @@
+    program toto
+
+     implicit none
+     integer ni,nj,nk,i,j,k,nelem,dsiz
+     real, target, allocatable, dimension(:,:) :: dbus
+     real, allocatable, dimension (:,:) :: u0,v0,t0
+     real, pointer, dimension(:) :: pbus
+     integer uplus,vplus,tplus
+     common /phybus_i/uplus,vplus,tplus
+     real, pointer, dimension (:,:) :: uplus0,vplus0,tplus0
+     ni=10
+     nj=5
+     nk=10
+     nelem=3
+     uplus=1
+     vplus=ni*nk+1
+     tplus=ni*nk*2 + 1
+     dsiz=ni*nk*nelem
+     allocate(dbus(ni*nk*nelem,nj))
+     allocate(u0(ni,nk))
+     allocate(v0(ni,nk))
+     allocate(t0(ni,nk))
+     do j=1,nj
+     pbus => dbus(:,j)
+     uplus0(1:ni,1:nk) => pbus(uplus:)
+     vplus0(1:ni,1:nk) => pbus(vplus:)
+     tplus0(1:ni,1:nk) => pbus(tplus:)
+     print *,'Set tplus= 240.0'
+     print *,'Set uplus= to 10.0'
+     print *,'Set vplus= to 5.0'
+     tplus0 = 240.0
+     uplus0 = 10.0
+     vplus0 = 5.0
+     u0 = uplus0
+     v0 = vplus0
+     t0 = tplus0
+     print *,'uplus0(1,1)=',uplus0(1,1),' u0(1,1)=',u0(1,1)
+     print *,'vplus0(1,1)=',vplus0(1,1),' v0(1,1)=',v0(1,1)
+     print *,'tplus0(1,1)=',tplus0(1,1),' t0(1,1)=',t0(1,1)
+     print *,'Set tplus(:,:)= 240.0'
+     print *,'Set uplus(:,:)= to 10.0'
+     print *,'Set vplus(:,:)= to 5.0'
+
+     tplus0(:,:) = 240.0
+     uplus0(:,:) = 10.0
+     vplus0(:,:) = 5.0
+     u0(:,:) = uplus0
+     v0(:,:) = vplus0
+     t0(:,:) = tplus0
+     print *,'uplus0(1,1)=',uplus0(1,1),' u0(1,1)=',u0(1,1)
+     print *,'vplus0(1,1)=',vplus0(1,1),' v0(1,1)=',v0(1,1)
+     print *,'tplus0(1,1)=',tplus0(1,1),' t0(1,1)=',t0(1,1)
+     enddo
+     stop
+     end
+
