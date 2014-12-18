ed1c6b7811e0c96676685b69abf0e89dd874ebc5
Michel Valin <mfvalin@gmail.com>
2014-12-18 23:39:25 +0000

    fixed indexing bug

--- EXTRAS/array_stats.c
+++ EXTRAS/array_stats.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-#ifdef __SSE2__
+#ifdef __AVX2__
 #include <immintrin.h>
 #endif
 
-#ifdef __SSE2__
+#ifdef __AVX2__
 void MinMaxSumsC(float *z_in, int n, float *Max, float *Min, float *Sum, float *Sum2)
 #else
 void MinMaxSums(float *z_in, int n, float *Max, float *Min, float *Sum, float *Sum2)
@@ -32,7 +32,7 @@ void MinMaxSums(float *z_in, int n, float *Max, float *Min, float *Sum, float *S
   *Sum2 = tsum2;
 }
 
-#ifdef __SSE2__
+#ifdef __AVX2__
 void MinMaxC(float *z_in, int n, float *Max, float *Min)
 #else
 void MinMax(float *z_in, int n, float *Max, float *Min)
@@ -53,7 +53,33 @@ void MinMax(float *z_in, int n, float *Max, float *Min)
   *Min  = tmin;
 }
 
-#ifdef __SSE2__
+#ifdef __AVX2__
+void MinMaxIndexC(float *z_in, int n, float *Max, float *Min, int *Imax, int *Imin)
+#else
+void MinMaxIndex(float *z_in, int n, float *Max, float *Min, int *Imax, int *Imin)
+#endif
+{
+  int i;
+  float tmax, tmin;
+  int imin, imax;
+
+  tmax = z_in[0];
+  tmin = z_in[0];
+  imax = 0;
+  imin = 0;
+
+  for (i=1 ; i<n ; i++){
+    if(z_in[i] > tmax) { tmax = z_in[i] ; imax = i ; };
+    if(z_in[i] < tmin) { tmin = z_in[i] ; imin = i ; };
+  }
+
+  *Max  = tmax;
+  *Min  = tmin;
+  *Imax = imax;
+  *Imin = imin;
+}
+
+#ifdef __AVX2__
 void MinMaxSums(float *z_in, int n, float *Max, float *Min, float *Sum, float *Sum2)
 {
   float zmin[4], zmax[4];
@@ -145,17 +171,19 @@ void MinMax(float *z_in, int n, float *Max, float *Min)
   float *z = z_in;
   __m256 z0, z1, z2, z3, zzmin, zzmax;
   int i;
-  int nm63 = n - 31;
+//  int nm63 = n - 31;
+  int nm63 = n - 15;
 
-  zzmin  = _mm256_loadu_ps(&z[00]);
-  zzmax  = _mm256_loadu_ps(&z[00]);
+  zzmin  = _mm256_loadu_ps(&z[0]);
+  zzmax  = _mm256_loadu_ps(&z[0]);
 
-  for (i = 0 ; i < nm63 ; i += 32){
+//  for (i = 0 ; i < nm63 ; i += 32){
+  for (i = 0 ; i < nm63 ; i += 16){
 
     z0 = _mm256_loadu_ps(&z[ 0]);
     z1 = _mm256_loadu_ps(&z[ 8]);
-    z2 = _mm256_loadu_ps(&z[16]);
-    z3 = _mm256_loadu_ps(&z[24]);
+//    z2 = _mm256_loadu_ps(&z[16]);
+//    z3 = _mm256_loadu_ps(&z[24]);
 
     zzmin = _mm256_min_ps(zzmin,z0);       // min
     zzmax = _mm256_max_ps(zzmax,z0);       // max
@@ -163,12 +191,13 @@ void MinMax(float *z_in, int n, float *Max, float *Min)
     zzmin = _mm256_min_ps(zzmin,z1);
     zzmax = _mm256_max_ps(zzmax,z1);
 
-    zzmin = _mm256_min_ps(zzmin,z2);
-    zzmax = _mm256_max_ps(zzmax,z2);
+//    zzmin = _mm256_min_ps(zzmin,z2);
+//    zzmax = _mm256_max_ps(zzmax,z2);
 
-    zzmin = _mm256_min_ps(zzmin,z3);
-    zzmax = _mm256_max_ps(zzmax,z3);
+//    zzmin = _mm256_min_ps(zzmin,z3);
+//    zzmax = _mm256_max_ps(zzmax,z3);
 
+//    z += 32;
     z += 16;
 
   }
@@ -200,6 +229,65 @@ void MinMax(float *z_in, int n, float *Max, float *Min)
   *Min=smin;
   return;
 }
+void MinMaxIndex(float *z_in, int n, float *Max, float *Min, int *Imax, int *Imin)
+{
+  float zmin[8], zmax[8];
+  int   imin[8], imax[8];
+  int   in07[8] = { 0, 1, 2, 3, 4, 5, 6, 7};
+  float smin, smax ;
+  int   xmin, xmax ;
+  float *z = z_in;
+  __m256 z0, zzmin, zzmax;
+  __m256i index, ixmax, ixmin, c8, msmin, msmax;
+  int i, j;
+  int nm63 = n - 7;
+
+  zzmin  = _mm256_loadu_ps(&z[0]);
+  zzmax  = _mm256_loadu_ps(&z[0]);
+  index  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+  ixmin  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+  ixmax  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+  c8     = _mm256_set1_epi32(8);
+
+  for (i = 0 ; i < nm63 ; i += 8){
+    z0 = _mm256_load_ps(z+i);
+
+    msmin = (__m256i)_mm256_cmp_ps(zzmin,z0,_CMP_GT_OS);
+    msmax = (__m256i)_mm256_cmp_ps(zzmax,z0,_CMP_LT_OS);
+    zzmin = _mm256_min_ps(zzmin,z0);
+    zzmax = _mm256_max_ps(zzmax,z0);
+    ixmin = _mm256_blendv_epi8(ixmin,index,msmin);
+    ixmax = _mm256_blendv_epi8(ixmax,index,msmax);
+    index = _mm256_add_epi32(index,c8);    // bump index
+  }
+  _mm256_storeu_ps(zmin,zzmin);
+  _mm256_storeu_ps(zmax,zzmax);
+  _mm256_storeu_si256((__m256i *)&imin[0],ixmin);
+  _mm256_storeu_si256((__m256i *)&imax[0],ixmax);
+
+  smax = zmax[0];
+  xmax = 0;
+  smin = zmin[0];
+  xmin = 0;
+
+  for (j=1 ; j<8 ; j++) {  
+    if(smax < zmax[j]) {smax = zmax[j] ; xmax = j ; }
+    if(smin > zmin[j]) {smin = zmin[j] ; xmin = j ; }
+  }
+  xmax = imax[xmax];
+  xmin = imin[xmin];
+
+  while(i<n){
+    if(smax < z_in[i]) { smax = z_in[i] ; xmax = i ; };
+    if(smin > z_in[i]) { smin = z_in[i] ; xmin = i ; };
+    i++ ;
+  }
+  *Max  = smax;
+  *Min  = smin;
+  *Imax = xmax;
+  *Imin = xmin;
+  return;
+}
 #endif
 #define NPTS (200000*1024+5)
 #include <sys/time.h>
@@ -209,15 +297,17 @@ main()
   long long T1, T2;
   int duree;
   float Z[NPTS];
-  float Max, Min, Sum, Div, Sum2;
+  float Max, Min, Sum, Sum2;
+  double Div;
+  int Imax, Imin;
   int i;
 
   Div = NPTS;
-  for(i=0 ; i<NPTS; i++) Z[i] = ((i-0.5*Div)/Div)*4.0*((i-0.5*Div)/Div);
+  for(i=0 ; i<NPTS; i++) Z[i] = (((i-0.5*Div)/Div)*4.0*((i-0.5*Div)/Div)) ;
+  Z[0] = .5 ; Z[3] = .5 ; Z[5] = .5 ; Z[7] = 0;
   printf("NPTS=%d, z[0]=%f,z[NPTS/2]=%f,z[NPTS-1]=%f\n",NPTS,Z[0],Z[NPTS/2],Z[NPTS-1]);
 
-#ifdef __SSE2__
-  printf("Test with sse2\n");
+#ifdef BENCH
   MinMaxSums(Z, NPTS, &Max, &Min, &Sum, &Sum2);
   MinMaxSums(Z, NPTS, &Max, &Min, &Sum, &Sum2);
   MinMaxSums(Z, NPTS, &Max, &Min, &Sum, &Sum2);
@@ -226,8 +316,10 @@ main()
   MinMaxSums(Z, NPTS, &Max, &Min, &Sum, &Sum2);
 #endif
 
+  printf("=================== C Timings ===================\n\n");
+
   gettimeofday(&t1,NULL);
-#ifdef __SSE2__
+#ifdef __AVX2__
   MinMaxSumsC(Z, NPTS, &Max, &Min, &Sum, &Sum2);
 #else
   MinMaxSums(Z, NPTS, &Max, &Min, &Sum, &Sum2);
@@ -240,7 +332,7 @@ main()
   printf("Min=%f, Max=%f, Sum=%f, Sum2=%f \n",Min,Max,Sum,Sum2);
 
   gettimeofday(&t1,NULL);
-#ifdef __SSE2__
+#ifdef __AVX2__
   MinMaxC(Z, NPTS, &Max, &Min);
 #else
   MinMax(Z, NPTS, &Max, &Min);
@@ -252,7 +344,23 @@ main()
   printf("MinMaxC time = %d usec, %dMtok/s\n",duree,NPTS/duree);
   printf("Min=%f, Max=%f \n",Min,Max);
 
-#ifdef __SSE2__
+  gettimeofday(&t1,NULL);
+#ifdef __AVX2__
+  MinMaxIndexC(Z, NPTS, &Max, &Min, &Imax, &Imin);
+#else
+  MinMaxIndex(Z, NPTS, &Max, &Min, &Imax, &Imin);
+#endif
+  gettimeofday(&t2,NULL);
+  T1 = t1.tv_sec ; T1 = T1*1000000 + t1.tv_usec ;
+  T2 = t2.tv_sec ; T2 = T2*1000000 + t2.tv_usec ;
+  duree = T2-T1;
+  printf("MinMaxIndexC time = %d usec, %dMtok/s\n",duree,NPTS/duree);
+  printf("Min=%f, Max=%f, Imin=%d, Imax=%d, %f, %f \n",Min,Max,Imin,Imax,Z[Imin],Z[Imax]);
+
+#ifdef __AVX2__
+
+  printf("=================== Accelerated Timings ===================\n\n");
+
   gettimeofday(&t1,NULL);
   MinMaxSums(Z, NPTS, &Max, &Min, &Sum, &Sum2);
   gettimeofday(&t2,NULL);
@@ -270,6 +378,17 @@ main()
   duree = T2-T1;
   printf("MinMax time = %d usec, %dMtok/s\n",duree,NPTS/duree);
   printf("Min=%f, Max=%f \n",Min,Max);
+
+  gettimeofday(&t1,NULL);
+  MinMaxIndex(Z, NPTS, &Max, &Min, &Imax, &Imin);
+  gettimeofday(&t2,NULL);
+  T1 = t1.tv_sec ; T1 = T1*1000000 + t1.tv_usec ;
+  T2 = t2.tv_sec ; T2 = T2*1000000 + t2.tv_usec ;
+  duree = T2-T1;
+  printf("MinMaxIndex time = %d usec, %dMtok/s\n",duree,NPTS/duree);
+  printf("Min=%f, Max=%f, Imin=%d, Imax=%d \n",Min,Max,Imin,Imax);
+  printf("Min=%f, Max=%f, Imin=%d, Imax=%d, %f, %f \n",Min,Max,Imin,Imax,Z[Imin],Z[Imax]);
+
 #endif
 
 }
