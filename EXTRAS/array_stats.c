66cf8f5688418bf33a5abc2ce854292faff7963e
Michel <armnmfv@nemesis.(none)>
2014-12-19 11:15:56 -0500

    added max,min,indexes,sums,sums2 routine

--- EXTRAS/array_stats.c
+++ EXTRAS/array_stats.c
@@ -90,14 +90,14 @@ void MinMaxSums(float *z_in, int n, float *Max, float *Min, float *Sum, float *S
   __m128 z0, z1, z2, z3, zzmin, zzmax, zzsum, zzsum2;
   __m256d y0, y1, y2, y3, yysum, yysum2;
   int i;
-  int nm63 = n - 15;
+  int limit = n - 15;
 
   zzmin  = _mm_loadu_ps(&z[00]);
   zzmax  = _mm_loadu_ps(&z[00]);
   yysum  = _mm256_set1_pd(0.0D);
   yysum2 = _mm256_set1_pd(0.0D);
 
-  for (i = 0 ; i < nm63 ; i += 16){
+  for (i = 0 ; i < limit ; i += 16){
 
     z0 = _mm_loadu_ps(&z[ 0]);
     z1 = _mm_loadu_ps(&z[ 4]);
@@ -171,14 +171,14 @@ void MinMax(float *z_in, int n, float *Max, float *Min)
   float *z = z_in;
   __m256 z0, z1, z2, z3, zzmin, zzmax;
   int i;
-//  int nm63 = n - 31;
-  int nm63 = n - 15;
+//  int limit = n - 31;
+  int limit = n - 15;
 
   zzmin  = _mm256_loadu_ps(&z[0]);
   zzmax  = _mm256_loadu_ps(&z[0]);
 
-//  for (i = 0 ; i < nm63 ; i += 32){
-  for (i = 0 ; i < nm63 ; i += 16){
+//  for (i = 0 ; i < limit ; i += 32){
+  for (i = 0 ; i < limit ; i += 16){
 
     z0 = _mm256_loadu_ps(&z[ 0]);
     z1 = _mm256_loadu_ps(&z[ 8]);
@@ -240,52 +240,157 @@ void MinMaxIndex(float *z_in, int n, float *Max, float *Min, int *Imax, int *Imi
   __m256 z0, zzmin, zzmax;
   __m256i index, ixmax, ixmin, c8, msmin, msmax;
   int i, j;
-  int nm63 = n - 7;
+  const int block = 8;
+  const int limit = n - block + 1;
 
-  zzmin  = _mm256_loadu_ps(&z[0]);
-  zzmax  = _mm256_loadu_ps(&z[0]);
-  index  = _mm256_loadu_si256((__m256i const *)&in07[0]);
-  ixmin  = _mm256_loadu_si256((__m256i const *)&in07[0]);
-  ixmax  = _mm256_loadu_si256((__m256i const *)&in07[0]);
-  c8     = _mm256_set1_epi32(8);
+  if(n < block) {
+    smax = z_in[0];
+    smin = smax;
+    xmin = 0;
+    xmax = 0;
+    i = 1;
+  }else{
+    zzmin  = _mm256_loadu_ps(&z[0]);
+    zzmax  = _mm256_loadu_ps(&z[0]);
+    index  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+    ixmin  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+    ixmax  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+    c8     = _mm256_set1_epi32(8);
 
-  for (i = 0 ; i < nm63 ; i += 8){
-    z0 = _mm256_load_ps(z+i);
+    for (i = 0 ; i < limit ; i += block){
+      z0 = _mm256_load_ps(z+i);
 
-    msmin = (__m256i)_mm256_cmp_ps(zzmin,z0,_CMP_GT_OS);
-    msmax = (__m256i)_mm256_cmp_ps(zzmax,z0,_CMP_LT_OS);
-    zzmin = _mm256_min_ps(zzmin,z0);
-    zzmax = _mm256_max_ps(zzmax,z0);
-    ixmin = _mm256_blendv_epi8(ixmin,index,msmin);
-    ixmax = _mm256_blendv_epi8(ixmax,index,msmax);
-    index = _mm256_add_epi32(index,c8);    // bump index
+      msmin = (__m256i)_mm256_cmp_ps(zzmin,z0,_CMP_GT_OS);
+      msmax = (__m256i)_mm256_cmp_ps(zzmax,z0,_CMP_LT_OS);
+      zzmin = _mm256_min_ps(zzmin,z0);
+      zzmax = _mm256_max_ps(zzmax,z0);
+      ixmin = _mm256_blendv_epi8(ixmin,index,msmin);
+      ixmax = _mm256_blendv_epi8(ixmax,index,msmax);
+      index = _mm256_add_epi32(index,c8);    // bump index
+    }
+    _mm256_storeu_ps(zmin,zzmin);
+    _mm256_storeu_ps(zmax,zzmax);
+    _mm256_storeu_si256((__m256i *)&imin[0],ixmin);
+    _mm256_storeu_si256((__m256i *)&imax[0],ixmax);
+
+    smax = zmax[0];
+    xmax = 0;
+    smin = zmin[0];
+    xmin = 0;
+
+    for (j=1 ; j<block ; j++) {  
+      if(smax < zmax[j]) {smax = zmax[j] ; xmax = j ; }
+      if(smin > zmin[j]) {smin = zmin[j] ; xmin = j ; }
+    }
+    xmax = imax[xmax];
+    xmin = imin[xmin];
   }
-  _mm256_storeu_ps(zmin,zzmin);
-  _mm256_storeu_ps(zmax,zzmax);
-  _mm256_storeu_si256((__m256i *)&imin[0],ixmin);
-  _mm256_storeu_si256((__m256i *)&imax[0],ixmax);
 
-  smax = zmax[0];
-  xmax = 0;
-  smin = zmin[0];
-  xmin = 0;
-
-  for (j=1 ; j<8 ; j++) {  
-    if(smax < zmax[j]) {smax = zmax[j] ; xmax = j ; }
-    if(smin > zmin[j]) {smin = zmin[j] ; xmin = j ; }
-  }
-  xmax = imax[xmax];
-  xmin = imin[xmin];
-
-  while(i<n){
+  for( ; i < n ; i++ ){
     if(smax < z_in[i]) { smax = z_in[i] ; xmax = i ; };
     if(smin > z_in[i]) { smin = z_in[i] ; xmin = i ; };
-    i++ ;
   }
   *Max  = smax;
   *Min  = smin;
   *Imax = xmax;
   *Imin = xmin;
+  return;
+}
+
+void MinMaxIndexSums(float *z_in, int n, float *Max, float *Min, int *Imax, int *Imin, float *Sum1, float *Sum2)
+{
+  float zmin[8], zmax[8];
+  int   imin[8], imax[8];
+  double ysum[4], ysum2[4];
+  int   in07[8] = { 0, 1, 2, 3, 4, 5, 6, 7};
+  float smin, smax;
+  double sum1, sum2 ;
+  int   xmin, xmax ;
+  float *z = z_in;
+  __m256 z0, zzmin, zzmax;
+  __m128 lo128, hi128;
+  __m256i index, ixmax, ixmin, c8, msmin, msmax;
+  __m256d lo128d, hi128d, sumd, sumsqr;
+  int i, j;
+  const int block = 8;
+  const int limit = n - block + 1;
+
+  if(n < block) {
+    smax = z_in[0];
+    smin = smax;
+    sum1 = smax;
+    sum2 = sum1 * sum1;
+    xmin = 0;
+    xmax = 0;
+    i = 1;
+  }else{
+    zzmin  = _mm256_loadu_ps(&z[0]);
+    zzmax  = _mm256_loadu_ps(&z[0]);
+    index  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+    ixmin  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+    ixmax  = _mm256_loadu_si256((__m256i const *)&in07[0]);
+    c8     = _mm256_set1_epi32(8);
+    sumd   = _mm256_set1_pd(0.0D);   // sums starts at 0.0
+    sumsqr = _mm256_set1_pd(0.0D);
+    sum1 = 0;
+    sum2 = 0;
+
+    for (i = 0 ; i < limit ; i += block){
+      z0    = _mm256_load_ps(z+i);
+      lo128 = _mm256_extractf128_ps(z0,0);
+      hi128 = _mm256_extractf128_ps(z0,1);
+      lo128d= _mm256_cvtps_pd(lo128);
+      hi128d= _mm256_cvtps_pd(hi128);
+
+      sumd  = _mm256_add_pd(sumd,lo128d);
+      lo128d= _mm256_mul_pd(lo128d,lo128d);
+      sumd  = _mm256_add_pd(sumd,hi128d);
+      hi128d= _mm256_mul_pd(hi128d,hi128d);
+      sumsqr= _mm256_add_pd(sumsqr,lo128d);
+      sumsqr= _mm256_add_pd(sumsqr,hi128d);
+
+      msmin = (__m256i)_mm256_cmp_ps(zzmin,z0,_CMP_GT_OS);
+      msmax = (__m256i)_mm256_cmp_ps(zzmax,z0,_CMP_LT_OS);
+      zzmin = _mm256_min_ps(zzmin,z0);
+      zzmax = _mm256_max_ps(zzmax,z0);
+      ixmin = _mm256_blendv_epi8(ixmin,index,msmin);
+      ixmax = _mm256_blendv_epi8(ixmax,index,msmax);
+      index = _mm256_add_epi32(index,c8);    // bump index
+    }
+    _mm256_storeu_ps(zmin,zzmin);
+    _mm256_storeu_ps(zmax,zzmax);
+    _mm256_storeu_pd(ysum,sumd);
+    _mm256_storeu_pd(ysum2,sumsqr);
+    _mm256_storeu_si256((__m256i *)&imin[0],ixmin);
+    _mm256_storeu_si256((__m256i *)&imax[0],ixmax);
+
+    smax = zmax[0];
+    xmax = 0;
+    smin = zmin[0];
+    xmin = 0;
+    sum1 = ysum[0]  + ysum[1]  + ysum[2]  + ysum[3] ;
+    sum2 = ysum2[0] + ysum2[1] + ysum2[2] + ysum2[3] ;
+
+    for (j=1 ; j<block ; j++) {  
+      if(smax < zmax[j]) {smax = zmax[j] ; xmax = j ; }
+      if(smin > zmin[j]) {smin = zmin[j] ; xmin = j ; }
+    }
+    xmax = imax[xmax];
+    xmin = imin[xmin];
+  }
+
+  for( ; i < n ; i++ ){
+    if(smax < z_in[i]) { smax = z_in[i] ; xmax = i ; };
+    if(smin > z_in[i]) { smin = z_in[i] ; xmin = i ; };
+    sum1 = sum1 + z_in[i];
+    sum2 = sum2 + z_in[i] * z_in[i];
+  }
+  *Max  = smax;
+  *Min  = smin;
+  *Imax = xmax;
+  *Imin = xmin;
+  *Sum1 = sum1;
+  *Sum2 = sum2;
   return;
 }
 #endif
@@ -388,6 +493,16 @@ main()
   printf("MinMaxIndex time = %d usec, %dMtok/s\n",duree,NPTS/duree);
   printf("Min=%f, Max=%f, Imin=%d, Imax=%d \n",Min,Max,Imin,Imax);
   printf("Min=%f, Max=%f, Imin=%d, Imax=%d, %f, %f \n",Min,Max,Imin,Imax,Z[Imin],Z[Imax]);
+
+  gettimeofday(&t1,NULL);
+  MinMaxIndexSums(Z, NPTS, &Max, &Min, &Imax, &Imin, &Sum, &Sum2);
+  gettimeofday(&t2,NULL);
+  T1 = t1.tv_sec ; T1 = T1*1000000 + t1.tv_usec ;
+  T2 = t2.tv_sec ; T2 = T2*1000000 + t2.tv_usec ;
+  duree = T2-T1;
+  printf("MinMaxIndexSums time = %d usec, %dMtok/s\n",duree,NPTS/duree);
+  printf("Min=%f, Max=%f, Imin=%d, Imax=%d \n",Min,Max,Imin,Imax);
+  printf("Min=%f, Max=%f, Imin=%d, Imax=%d, Sum=%f, Sum2=%f \n",Min,Max,Imin,Imax,Sum,Sum2);
 
 #endif
 
