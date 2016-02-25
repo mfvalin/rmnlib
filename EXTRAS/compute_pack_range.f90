bad68519de694cf0208e8bc2ef8c0791dcade089
Michel Valin <mfvalin@gmail.com>
2016-02-25 18:12:41 -0500

    testing low value rounding

--- EXTRAS/compute_pack_range.f90
+++ EXTRAS/compute_pack_range.f90
@@ -1,17 +1,36 @@
 program inv
-  integer :: i, power
-  real :: R, range,delta
+  integer :: nbits, power, izero, itop, ilow
+  real :: R, rr, rrange,delta, low, high, newlow
+1 read(5,*,err=2) low, high
   power = 2
-  delta = .55
-  range = 1234.56
-  range = 4096
-  do i = 2 , 16
+  delta = 2.01
+  rrange = 1234.56
+  rrange = high - low
+  do nbits = 2 , 16
+    rrange = high - low
     power = power * 2
-!   range = power * 19.5
-    r = range/(power - delta)
-    r = 1.0 / R
-    print *,power,delta,range,r,nint(range*R),power-1-range*R,power-nint(range*R)-1
-    if(delta < 2) delta = delta * 1.4
+!   rrange = power * 19.5
+    if(nbits>2) then
+      rr = rrange/(power - delta)
+    else
+      rr = rrange/(power - 1.01)
+    endif
+    r = 1.0 / rr
+    if(low<0 .and. high > 0) then
+      izero = nint((0-low)*r)
+      newlow = -rr*izero
+!      print *,'newlow=',newlow
+      izero = nint((0-newlow)*r)
+    else
+      newlow = low
+      izero = 0
+    endif
+    ilow = nint((low-newlow)*r)
+    itop = nint((high-newlow)*r)
+    print *,power,rrange,rr,newlow,nint(rrange*R),power-1-rrange*R,power-itop-1,newlow + izero*rr, &
+            newlow + itop*rr,itop,izero,ilow*rr+newlow
+!    if(delta < 2.1) delta = delta * 1.02
   enddo
-  stop
+  goto 1
+2 stop
 end
