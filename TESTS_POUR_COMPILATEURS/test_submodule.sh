0acc13f033007984fcab9073121d65f0fb5cf517
M. Valin <mfvalin@gmail.com>
2021-01-12 17:27:22 -0500

    added user defined type to the test

--- TESTS_POUR_COMPILATEURS/test_submodule.sh
+++ TESTS_POUR_COMPILATEURS/test_submodule.sh
@@ -2,6 +2,10 @@
 rm -f *.o *mod test_submodule.Abs
 cat <<EOT >module_source.F90
 module foo
+  type :: custom
+    integer :: v
+    real    :: z
+  end type
   interface bar
     module subroutine bar_integer(x)
       integer(kind=4), intent(in) :: x
@@ -11,6 +15,9 @@ module foo
     end subroutine
     module subroutine bar_real8(x)
       real(kind=8), intent(in) :: x
+    end subroutine
+    module subroutine bar_custom(x)
+      type(custom), intent(in) :: x
     end subroutine
   end interface
 end module
@@ -28,6 +35,9 @@ contains
   module procedure bar_real8
     print *,'bar_real8   :',x
   end procedure
+  module procedure bar_custom
+    print *,'bar_custom  :',x%v,x%z
+  end procedure
 end submodule
 EOT
 #
@@ -39,12 +49,15 @@ use foo
   call bar(i)
   call bar(r)
   call bar(1.2345_8)
+  call bar(custom(12345,5.4321))
   print *,'exiting main'
 end program
 EOT
-${1:-ftn} module_source.F90 submodule_source.F90 main_source.F90 -o test_submodule.Abs
+${1:-ftn} -c module_source.F90 submodule_source.F90 
+${1:-ftn} module_source.o submodule_source.o main_source.F90 -o test_submodule.Abs
 ./test_submodule.Abs
 ls -alrt | grep -i 'mod$' | tail -5
+ls -al module_source.o submodule_source.o
 rm -f module_source.F90 submodule_source.F90 main_source.F90 *.o *mod test_submodule.Abs
 # pgi, nvidia, flang : foo.mod,          foo-sub.mod
 # xlf                : foo.mod,          foo_sub.smod
