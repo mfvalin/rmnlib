405f347eaef6b4b59fd8b2758e21bc1133eee604
M. Valin <mfvalin@gmail.com>
2021-08-18 18:16:45 -0400

    assumed length character string example using subroutine creation

--- TESTS_POUR_COMPILATEURS/assumed_length_string.F90
+++ TESTS_POUR_COMPILATEURS/assumed_length_string.F90
@@ -1,6 +1,14 @@
 program str_var
   implicit none
+  interface
+  subroutine make_string(str, lng)
+    implicit none
+    character(len=:), pointer, intent(OUT) :: str
+    integer, intent(IN) :: lng
+  end subroutine make_string
+  end interface
   character(len=:), allocatable, target :: alloc_name
+  character(len=:), pointer :: alloc_name3
   character(len=:), pointer :: alloc_name2
   character(len=:), pointer :: ptr_name
 
@@ -13,4 +21,14 @@ program str_var
   print *, 'alloc_name ="'//alloc_name//'"'
   print *, 'len(alloc_name2) =',len(alloc_name2)
   print *, 'alloc_name2 ="'//alloc_name2//'"'
+  call make_string(alloc_name3, 12)
+  print *, 'len(alloc_name3) =',len(alloc_name3)
+  print *, 'alloc_name3 ="'//alloc_name3//'"'
 end program
+subroutine make_string(str, lng)
+  implicit none
+  character(len=:), pointer, intent(OUT) :: str
+  integer, intent(IN) :: lng
+  allocate(character(len=lng) :: str)
+  str = ' '
+end subroutine make_string
