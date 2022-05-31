8247a4e5d8adc066bbdc99c7b7427750c13adffb
M. Valin <mfvalin@gmail.com>
2022-05-31 10:42:11 -0400

    show variable initialization vs type initialisation

---
+++ TESTS_POUR_COMPILATEURS/test_initialisations.F90
@@ -0,0 +1,23 @@
+program test_initialisation
+  implicit none
+  integer :: val
+  call sub(val)
+  print *,'----------'
+  call sub(val)
+  print *,'----------'
+  call sub(val)
+end program
+subroutine sub(val)
+  implicit none
+  integer, intent(INOUT) :: val
+  type :: machin
+    integer :: v = 12
+  end type
+  type(machin) :: a = machin(13)  ! variable initialisation
+  type(machin) :: b               ! type initialisation
+  print *,'variable initialisee    machin a =',a%v
+  print *,'type initialise (avant) machin b =',b%v
+  a%v = a%v + 1
+  b%v = b%v + 1
+  print *,'type initialise (apres) machin b =',b%v
+end subroutine
