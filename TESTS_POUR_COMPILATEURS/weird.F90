19f060afc24b8202436ac8394c34453ab1ce8b93
M. Valin <mfvalin@gmail.com>
2023-04-21 09:53:42 -0400

    strange side effect of the preprocessor with some Fortran compielrs

---
+++ TESTS_POUR_COMPILATEURS/weird.F90
@@ -0,0 +1,19 @@
+module rmn_fstd98
+  use ISO_C_BINDING
+  implicit none
+
+  type :: fstd98
+    integer     :: iun = -1                  ! Fortran unit number
+  end type
+
+  interface
+    ! /*
+    module function fstd98_is_rsf(this) result(is_rsf2)
+      implicit none
+      type(fstd98), intent(IN) :: this
+      logical :: is_rsf2
+    end function fstd98_is_rsf
+  end interface
+    ! */
+
+end module rmn_fstd98
