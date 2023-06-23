969d56f15f9167e57acf1a743da4e559701f3f95
M. Valin <mfvalin@gmail.com>
2023-06-23 18:13:12 -0400

    simple polymorphic types

---
+++ TESTS_POUR_COMPILATEURS/polymorphic.F90
@@ -0,0 +1,46 @@
+module demo_poly
+use ISO_C_BINDING
+interface machin
+  subroutine machin1(arg)
+  real, intent(IN) :: arg
+  end subroutine
+  subroutine machin2(arg)
+  integer, intent(IN) :: arg
+  end subroutine
+end interface
+type :: truc
+    type(C_PTR) :: p
+contains
+    procedure :: procedure1
+    procedure :: procedure2
+    GENERIC   :: procedure => procedure1, procedure2
+end type
+
+contains
+  subroutine procedure1(dum, arg)
+  class(truc), intent(IN) :: dum
+  real, intent(IN) :: arg
+  print *,arg
+  end subroutine
+  subroutine procedure2(dum, arg)
+  class(truc), intent(IN) :: dum
+  integer, intent(IN) :: arg
+  print *,arg
+  end subroutine
+end module
+program demo
+use demo_poly
+type(truc) :: t
+call machin(234)
+call machin(2.34)
+call t%procedure(123)
+call t%procedure(1.23)
+end
+subroutine machin1(arg)
+real, intent(IN) :: arg
+  print *,arg
+end subroutine
+subroutine machin2(arg)
+integer, intent(IN) :: arg
+  print *,arg
+end subroutine
