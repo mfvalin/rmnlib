0db1a14c81b6f9ef599e4a33548c8a51d3246349
M. Valin <mfvalin@gmail.com>
2024-06-12 10:52:42 -0400

    array rank test

--- TESTS_POUR_COMPILATEURS/test_rank_intrinsic.F90
+++ TESTS_POUR_COMPILATEURS/test_rank_intrinsic.F90
@@ -1,11 +1,21 @@
 module test_rank_mod
   implicit none
+#if defined(C_RANK)
+  interface rank
+    function c_rank(this) result(n) BIND(C,name='rank_2018')
+      integer, dimension(..), intent(IN) :: this
+      integer :: n
+    end function
+  end interface
+#endif
 contains
   subroutine get_rank(this, expected)
     integer, dimension(..) :: this
     integer :: expected
     if(rank(this) .ne. expected) then
       print '(A,I2,A,I2)', 'ERROR: rank = ',rank(this), ' , expected ',expected
+    else
+      print '(A,I2,A,I2)', 'O.K.: rank = ',rank(this), ' , as expected '
     endif
   end
 end module
